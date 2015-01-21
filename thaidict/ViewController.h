bea762f65975d1df2252d68d89369315ec21008a
rnut <rnut.ist@gmail.com>
2015-01-21 14:31:57 +0700

    Initial Commit

---
+++ thaidict/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thaidict
+//
+//  Created by Rnut on 1/21/2558 BE.
+//  Copyright (c) 2558 Rnut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ thaidict/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  thaidict
+//
+//  Created by Rnut on 1/21/2558 BE.
+//  Copyright (c) 2558 Rnut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
